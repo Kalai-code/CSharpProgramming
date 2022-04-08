b29b0615681087d6d5b01e5731551f13bbd59a93
Kalaichitra Rangasamy <kalai.workmailbox@gmail.com>
2022-04-08 12:36:04 -0500

    Added solution for string problems

---
+++ InterviewProblemSolutions/ArraySolutions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            int num = prog.MaxSubArray();
+            Console.WriteLine(num);
+            Console.ReadKey();
+        }
+        public int MaxSubArray()
+        {
+            int[] nums = { -2, 1 };
+            int Maxsum = nums.Max();
+            int iSum = 0;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                iSum = nums[i];
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    iSum = iSum + nums[j];
+                    if (iSum > Maxsum)
+                    {
+                        Maxsum = iSum;
+                    }
+                }
+            }
+            return Maxsum;
+        }
+    }
+}
---
+++ InterviewProblemSolutions/stringproblem_solutions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringproblem_solutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            int[] output = prog.TwoSum();
+            Console.ReadKey();
+        }
+
+        public int[] TwoSum()
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            int[] output = { 0, 0 };
+            for (int i = 0; i <= nums.Length - 2; i++)
+            {
+                for (int j = i + 1; j <= nums.Length - 1; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        output = new int[] { i, j };
+                        return output;
+                    }
+                }
+            }
+            return output;
+        }
+    }
+}
